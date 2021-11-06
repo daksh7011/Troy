c0738358f5092284da66655aa390c89f26a2a52c
Daksh <daksh@technowolf.in>
2021-11-06 15:23:51 +0530

    adjust Reboot.kt

--- src/main/kotlin/commands/mod/Reboot.kt
+++ src/main/kotlin/commands/mod/Reboot.kt
@@ -2,6 +2,7 @@ package commands.mod
 
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.chatCommand
+import com.kotlindiscord.kord.extensions.utils.dm
 import dev.kord.core.Kord
 import org.koin.core.component.inject
 import utils.Extensions.isOwner
@@ -22,6 +23,10 @@ class Reboot : Extension() {
                     message.channel.createMessage("This is owner only command.")
                 } else {
                     message.channel.createMessage("Commencing reboot.")
+                    message.author?.dm(
+                        "Commencing restart process!\n" +
+                                "**Beware, If I am not running via pm2, You need to do start the process manually.**"
+                    )
                     kordClient.logout()
                     kordClient.shutdown()
                 }
